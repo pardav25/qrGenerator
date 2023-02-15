ff89f7085603385aafb4c30a9b36fdc7c8186e67
Davide Paradiso <dparadiso25@gmail.com>
2023-02-15 19:55:27 +0100

    upload application v1.0

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# qrGenerator
-Application to generate a QR code based on the text entered. You have the posibility to download the generated QR image or share it.
+[![LinkedIn][linkedin-shield]][linkedin-url]
+[![Facebook][facebook-shield]][facebook-url]
+[![Instagram][instagram-shield]][instagram-url]
+
+<div id="top"></div>             
+                    
+# QR Generator
+Web application to generate a QR code based on the text entered. You have the posibility to download the generated QR image or share it. 
+
+## Tools used
+- Visual Studio Code
+- GitHub Desktop 
+
+## Javascript libraries used
+- qrcodejs
+
+## License & Copyright
+
+<div align="center">
+  <h3 align="center">&copy; <a href="https://davideparadiso.ch/">Davide Paradiso </a></h3>
+</div> 
+
+
+<!-- MARKDOWN LINKS & IMAGES -->
+[linkedin-shield]: https://img.shields.io/badge/-LinkedIn-black.svg?style=for-the-badge&logo=linkedin&color=blue&colorB=white
+[facebook-shield]: https://img.shields.io/badge/-Facebook-black.svg?style=for-the-badge&logo=facebook&colorB=white
+[instagram-shield]: https://img.shields.io/badge/-Instagram-black.svg?style=for-the-badge&logo=instagram&colorB=white
+[linkedin-url]: https://www.linkedin.com/in/davide-paradiso-585bb717b
+[facebook-url]: https://www.facebook.com/profile.php?id=100081916540134
+[instagram-url]: https://www.instagram.com/davideparadiso.ch/
---
+++ js/index.js
@@ -0,0 +1,97 @@
+const download = document.querySelector(".download");
+const dark = document.querySelector(".dark");
+const light = document.querySelector(".light");
+const qrContainer = document.querySelector("#qr-code");
+const qrText = document.querySelector(".qr-text");
+const shareBtn = document.querySelector(".share-btn");
+const sizes = document.querySelector(".sizes");
+const popup = document.querySelector(".popup");
+const popupBtn = document.querySelector("#btn-popup");
+
+dark.addEventListener("input", handleDarkColor);
+light.addEventListener("input", handleLightColor);
+qrText.addEventListener("input", handleQRText);
+sizes.addEventListener("input", handleSize);
+shareBtn.addEventListener("click", handleShare);
+popupBtn.addEventListener("click", closePopup);
+
+const defualtUrl = "https://davideparadiso.ch";
+let colorLight = "#fff",
+    colorDark = "#000",
+    text = defualtUrl,
+    size = 300;
+
+function handleDarkColor(e) {
+    colorDark = e.target.value;
+    generateQRCode();
+}
+
+function handleLightColor(e) {
+    colorLight = e.target.value;
+    generateQRCode();
+}
+
+function handleQRText(e) {
+    const value = e.target.value;
+    text = !value ? defualtUrl : value;
+    generateQRCode();
+}
+
+async function generateQRCode() {
+    qrContainer.innerHTML = "";
+    new QRCode("qr-code", {
+        text,
+        height: size,
+        width: size,
+        colorLight,
+        colorDark
+    });
+    download.href = await resolveDataUrl();
+}
+
+async function handleShare() {
+    setTimeout(async() => {
+        try {
+            const base64url = await resolveDataUrl();
+            const blob = await (await fetch(base64url)).blob();
+            const file = new File([blob], "QR-Code.png", {
+                type: blob.type,
+            });
+            await navigator.share({
+                files: [file],
+                title: text,
+            });
+        } catch (error) {
+            openPopup();
+        }
+    }, 100);
+}
+
+function handleSize(e) {
+    size = e.target.value;
+    generateQRCode();
+}
+
+function resolveDataUrl() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const img = document.querySelector("#qr-code img");
+            if (img.currentSrc) {
+                resolve(img.currentSrc);
+                return
+            }
+            const canvas = document.querySelector("canvas");
+            resolve(canvas.toDataURL());
+        }, 50);
+    });
+}
+
+function openPopup() {
+    popup.classList.add("open-popup");
+}
+
+function closePopup() {
+    popup.classList.remove("open-popup");
+}
+
+generateQRCode();
